7308bd23ba3e04ab09ad535331c93829de5a5ac1
vzvenyach <vdavez@gmail.com>
2014-12-31 14:10:44 -0500

    STATIC_ROOT

--- legalsuff/settings.py
+++ legalsuff/settings.py
@@ -87,6 +87,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = ('/app/static/',)
 MEDIA_ROOT = 'media'
